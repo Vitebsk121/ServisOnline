34023a75c0e787d036bb9771ff9c62dbd7dd0dac
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-01-12 11:46:25 +0300

    feat: добавлено управление состоянием меню и страница заказов

--- src/Containers/App/App.tsx
+++ src/Containers/App/App.tsx
@@ -3,16 +3,30 @@ import "./App.scss";
 import React from "react";
 import Navigation from "../../Components/Navigation/Navigation";
 import Header from "../../Components/Header/Header";
+import OrderPage from "../Pages/OrderPage/OrderPage";
+import {useDispatch} from "react-redux";
+import useTypedSelector from "../../hooks/useTypedSelector";
+import {closeMenu} from "../../store/actions/appActions";
+import Backdrop from "../../Components/UI/Backdrop/Backdrop";
 
 type AppProps = {};
 
 const App: React.FC<AppProps> = (props: AppProps) => {
+
+  const dispatch = useDispatch();
+  const { menuIsOpen } = useTypedSelector((state) => state.appState);
+
+  const menuHandler = () => {
+    menuIsOpen ? dispatch(closeMenu()) : null;
+  };
+
   return (
     <div className="app__wrapper mainTheme">
       <Navigation />
-      <div className="page__content__wrapper">
+      <Backdrop />
+      <div role='none' className="page__content__wrapper" onClick={menuHandler}>
         <Header pageTitle={'Заказы'} storeTitle={['v8 Витебск']} />
-        {/*<Page />*/}
+        <OrderPage/>
       </div>
     </div>
   );
