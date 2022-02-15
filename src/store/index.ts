52f100d4c8938d74de52b5bb3cc8e9bed46bd67a
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-02-15 16:44:29 +0300

    fix: редакс девтулс отключен

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,7 @@ const store = createStore(
   compose(
     applyMiddleware(thunk),
     // @ts-ignore
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
 
