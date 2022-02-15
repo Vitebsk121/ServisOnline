3a46cb91671894ce0b6327fdf8d1b5036db375fe
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-02-15 16:14:56 +0300

    feat: на страницу заказов добавлена таблица заказов, и бэкдроп

--- src/Containers/Pages/OrderPage/OrderPage.tsx
+++ src/Containers/Pages/OrderPage/OrderPage.tsx
@@ -3,14 +3,18 @@ import "./OrderPage.scss";
 import React from "react";
 import Controls from "../../../Components/ControlsOrderPage/Controls";
 import NewOrder from "../../../Components/NewOrder/NewOrder";
+import OrdersList from "../../../Components/OrdersList/OrdersList";
+import useTypedSelector from "../../../hooks/useTypedSelector";
 
 type OrderPageProps = {};
 
 const OrderPage: React.FC<OrderPageProps> = (props: OrderPageProps) => {
+  const { backDropIsOpen } = useTypedSelector((state) => state.appState);
   return (
     <div className='orderPage'>
       <Controls />
-      <NewOrder />
+      {backDropIsOpen ? <NewOrder /> : null}
+      <OrdersList />
     </div>
   );
 };
